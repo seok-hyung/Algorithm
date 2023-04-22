a8cafa15b7fbfb4feedbfadadb99a28760ab6b7a
seokhyungth <korea970223@naver.com>
2023-04-22 23:28:44 +0900

    프로그래머스 level 0

---
+++ 문자열계산하기.js
@@ -0,0 +1,15 @@
+// 문자열 계산하기 
+function solution(my_string) {
+    let arr = my_string.split(' ')
+    let answer = arr[0]*1
+    arr.forEach((item,index)=>{
+        if(item ==='+'){
+            answer += parseInt(arr[index+1])
+        }
+        if(item === '-'){
+            answer -= parseInt(arr[index+1])
+        }
+    })
+    return answer
+}
+
