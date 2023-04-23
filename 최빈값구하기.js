38a624d65f69528e34d279a7eb70999d7eebf306
seokhyungth <korea970223@naver.com>
2023-04-23 23:49:16 +0900

    프로그래머스 level 0

---
+++ 최빈값구하기.js
@@ -0,0 +1,23 @@
+function solution(array) {
+    var answer = 0;
+    let a = [];
+    let temp = 0
+
+    for(let i=0; i<array.length; i++){
+        if(a.includes(array[i])==false){
+            a.push(array[i])
+        }
+    }
+    for(let i=0; i<a.length; i++){
+        const count = array.filter(element => a[i] == element).length
+        if(count>temp){
+            temp = count
+            answer = a[i]
+        }
+        else if(count == temp){
+            answer = -1
+        }
+    }
+    return answer;
+}
+
