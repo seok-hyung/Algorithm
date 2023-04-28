5b2682b16350f4fc3d04c230922a94ef1cd0cbb9
DESKTOP-KRJ342G\korea <korea970223@naver.com>
2023-04-28 23:26:35 +0900

    프로그래머스 level 0

---
+++ 다음에올숫자.js
@@ -0,0 +1,10 @@
+function solution(common){
+    if(common[2] - common[1] === common[1] - common[0]){
+        return common.pop() + common[1] - common[0]
+    }else{
+        return common.pop() *(common[1] / common[0])
+    }
+}
+
+console.log(solution([1,2,3,4])) // 5
+console.log(solution([2,4,8])) // 16
