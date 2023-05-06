6234a2c4c224ac7f682a2704b3c060095d7034ee
Seokhyung Lee <123619657+seok-hyung@users.noreply.github.com>
2023-05-06 23:49:48 +0900

    Update 7의개수.js

--- 7의개수.js
+++ 7의개수.js
@@ -1,7 +1,6 @@
-let a =[7,77,17]
-
 function solution(array){
     return array.join('').split('').filter((v)=>v==='7').length
 }
 
-console.log(solution([7,77,17]))
+console.log(solution([7,77,17])) // 4
+console.log(solution([10,29])) // 0
