4cf7c23f8c7e1c08a11bd8e37de03203eb1dda5f
Seokhyung Lee <123619657+seok-hyung@users.noreply.github.com>
2023-05-21 23:56:30 +0900

    프로그래머스 level 0

---
+++ 특이한정렬.js
@@ -0,0 +1,17 @@
+// 나의 풀이
+function solution(numList, n) {
+    let result = []
+    result = numList.sort((a, b) => {
+        const [numA, numB] = [Math.abs(a - n), Math.abs(b - n)];
+        if(numA === numB){
+            return b - a;
+        }
+        return numA - numB;
+    })
+    return result;
+}
+
+// 다른 사람의 풀이
+function solution(numlist, n) {
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
+}
