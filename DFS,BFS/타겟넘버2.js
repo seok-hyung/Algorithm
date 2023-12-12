a01283073af2815a1f1a712fa06c04913719cb3b
seok-hyung <korea970223@naver.com>
2023-12-12 23:10:27 +0900

    스택을 활용한 DFS 풀이 추가

--- DFS,BFS/타겟넘버2.js
+++ DFS,BFS/타겟넘버2.js
@@ -1,27 +1,53 @@
 // 타겟 넘버 재풀이
+
+// 1. 재귀함수를 사용한 풀이
 function solution(numbers, target) {
   let answer = 0;
-
+  dfs(0, 0);
   function dfs(index, sum) {
-    // 실행할 코드
-    // 탈출 조건
-    // 인덱스값이 numbers의 길이가 되었을때
-    // sum 값이 target값이 되었을 때
-    dfs(0, 0);
-    // 탈출조건
-    if (index === numbers.length) {
+    if (index < numbers.length) {
+      // 아직 사용하지 않은 숫자가 있는 경우
+      dfs(index + 1, sum + numbers[index]);
+      dfs(index + 1, sum - numbers[index]);
+    } else {
+      // 모든 숫자를 사용한 경우
       if (sum === target) {
         answer++;
       }
       return;
     }
-    dfs(index + 1, sum + numbers[index]);
-    dfs(index + 1, sum - numbers[index]);
   }
 
   return answer;
 }
+console.log(solution([4, 1, 2, 1], 4));
 
-// 모든 경우의 수를 따지고 나옴값이 target이 되면 +1
-// 일단 다 더한다, 끝나면 한 턴 돌아와서 빼준다.
-//
+// 2. 스택을 활용한 풀이
+function solution2(numbers, target) {
+  let answer = 0;
+  let stack = [
+    [numbers[0], 0],
+    [-numbers[0], 0],
+  ]; // 초기값 설정
+
+  while (stack.length) {
+    const [sum, index] = stack.pop();
+
+    // 모든 숫자를 다 사용한 경우
+    if (index === numbers.length - 1) {
+      // 합계가 타겟과 같은 경우
+      if (sum === target) {
+        answer++;
+      }
+    } else {
+      // 아직 사용하지 않은 숫자가 있는 경우
+      const nextIndex = index + 1;
+      const nextNumber = numbers[nextIndex];
+
+      stack.push([sum + nextNumber, nextIndex]); // 다음 숫자를 더한 경우
+      stack.push([sum - nextNumber, nextIndex]); // 다음 숫자를 뺀 경우
+    }
+  }
+
+  return '답은 : ' + answer;
+}
