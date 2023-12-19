cce7db28f6b95ca50893cd8b857551fdd17c97c4
seok-hyung <korea970223@naver.com>
2023-12-19 23:41:14 +0900

    에라토네스의 체 알고리즘으로 소수찾기

--- 소수찾기.js
+++ 소수찾기.js
@@ -1,23 +1,10 @@
-// 프로그래머스 완전탐색 level 1
+// 에라토스테네스의 체
 function solution(n) {
-  let answer = 0;
-  const arr = new Array(n + 1).fill(true);
-
-  for (let i = 2; i <= n; ++i) {
-    // 이미 소수가 아닌 인덱스는 건너뛴다.
-    if (arr[i] === false) {
-      continue;
-    }
-    // 배수는 소수가 아니라 0으로 설정
-    for (let k = i * 2; k <= n; k += i) {
-      arr[k] = false;
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
     }
   }
-  // 소수의 갯수를 구한다.
-  for (let i = 2; i <= n; ++i) {
-    if (arr[i] === true) {
-      answer++;
-    }
-  }
-  return answer;
+  return true;
 }
+// 매개변수를 n을 인자로 받고, 2부터 n의 제곱근까지의 수로 n을 나눴을떄, 0으로 나눠떨어지는 수가 있다면 소수가 아니라고 판단합니다.
