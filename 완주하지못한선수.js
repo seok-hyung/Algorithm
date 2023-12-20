653ece27fd415f3b286438c0c8157ddc33acf824
seok-hyung <korea970223@naver.com>
2023-12-20 14:00:24 +0900

    hash 사용한 풀이 추가

--- 완주하지못한선수.js
+++ 완주하지못한선수.js
@@ -1,12 +1,52 @@
-// 프로그래머스 해시 level 2
+// 프로그래머스 해시 level 1
+
+// 첫번쨰 풀이, 해쉬사용 X
 function solution(participant, completion) {
-  participant.sort(); //참가자 배열 정렬
-  completion.sort(); //완주자 배열 정렬
+  participant.sort();
+  completion.sort();
   for (var i = 0; i < participant.length; i++) {
     if (participant[i] !== completion[i]) {
-      //인덱스 0부터 순차적으로 두 배열 비교
       return participant[i];
-      //비완주자가 참가자 배열에 나올 경우 출력
     }
+  }
+}
+
+// 두번째 풀이, 해쉬 사용 O
+function solution2(participant, completion) {
+  const myMap = new Map();
+
+  for (const participant of participants) {
+    if (!myMap.get(participant)) {
+      myMap.set(participant, 1);
+    } else {
+      myMap.set(participant, myMap.get(participant) + 1);
+    }
+  }
+
+  for (const completion of completions) {
+    if (myMap.get(completion)) {
+      myMap.set(completion, myMap.get(completion) - 1);
+    }
+  }
+
+  for (const participant of participants) {
+    if (myMap.get(participant) && myMap.get(participant) >= 1) {
+      answer = participant;
+    }
+  }
+}
+
+// 세번째 풀이, 해쉬 사용 O (두번쨰 풀이와 동일함)
+function solution3(participant, completion) {
+  let hashed = [];
+  participant.forEach((entry) => {
+    hashed[entry] = hashed[entry] ? hashed[entry] + 1 : 1;
+  });
+  completion.forEach((entry) => {
+    hashed[entry] = hashed[entry] - 1;
+  });
+
+  for (var key in hashed) {
+    if (hashed[key] >= 1) return key;
   }
 }
