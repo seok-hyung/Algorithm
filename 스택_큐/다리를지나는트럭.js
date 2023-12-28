117ee8d3f421725aaffca00010e87aefc4d9c496
seok-hyung <korea970223@naver.com>
2023-12-28 22:16:36 +0900

    재풀이 및 다른사람 코드 추가

--- 스택_큐/다리를지나는트럭.js
+++ 스택_큐/다리를지나는트럭.js
@@ -1,22 +1,42 @@
+// 프로그래머스 스택/큐 level 2
 function solution(bridge_length, weight, truck_weights) {
-  var answer = 0;
-  let bridge = [];
-  let bridge_weight = 0;
+  const bridge = Array.from({ length: bridge_length }, () => 0);
+  let curWeight = 0;
+  let time = 0;
   while (truck_weights.length > 0) {
-    answer++;
-    if (bridge.length == bridge_length) {
-      bridge_weight -= bridge.shift();
-    }
-    if (bridge_weight + truck_weights[0] > weight) {
+    time++;
+    curWeight -= bridge.shift();
+
+    if (curWeight + truck_weights[0] <= weight) {
+      const curTruck = truck_weights.shift();
+      bridge.push(curTruck);
+      curWeight += curTruck;
+    } else {
       bridge.push(0);
-      continue;
     }
-    let truck_weight = truck_weights.shift();
-    bridge.push(truck_weight);
-    bridge_weight += truck_weight;
   }
+  return time + bridge_length;
+}
 
-  answer += bridge_length;
+function solution2(bridge_length, weight, truck_weights) {
+  let answer = 0;
+  let bridge = Array.from({ length: bridge_length }, () => 0);
+  let bridge_sum = 0;
 
+  answer++;
+  bridge.shift();
+  bridge_sum += truck_weights[0];
+  bridge.push(truck_weights.shift());
+
+  while (bridge_sum > 0) {
+    answer++;
+    bridge_sum -= bridge.shift();
+    if (truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight) {
+      bridge_sum += truck_weights[0];
+      bridge.push(truck_weights.shift());
+    } else {
+      bridge.push(0);
+    }
+  }
   return answer;
 }
