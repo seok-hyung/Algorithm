da975dc3597d93f07e4f3d6955342b3de4831156
seok-hyung <korea970223@naver.com>
2023-12-29 20:07:01 +0900

    스택,큐 각각을 활용한 풀이 추가

--- 스택_큐/주식가격.js
+++ 스택_큐/주식가격.js
@@ -1,4 +1,5 @@
-// 프로그래머스 스택/큐 level 2
+// 프로그래머스 스택&큐 level 2
+
 function solution(prices) {
   const answer = [];
   for (let i = 0; i < prices.length; i++) {
@@ -13,3 +14,48 @@ function solution(prices) {
   }
   return answer;
 }
+
+// 스택을 활용한 풀이
+function solution2(prices) {
+  let answer = new Array(prices.length).fill(0);
+  let stack = [];
+
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+      let top = stack.pop();
+      answer[top] = i - top;
+    }
+    stack.push(i);
+  }
+
+  while (stack.length > 0) {
+    let top = stack.pop();
+    answer[top] = prices.length - 1 - top;
+  }
+
+  return answer;
+}
+
+// 큐를 활용한 풀이
+function solution3(prices) {
+  let answer = new Array(prices.length).fill(0);
+  let queue = prices.map((price, index) => ({ price, index }));
+
+  console.log(answer);
+  console.log(queue);
+  while (queue.length > 0) {
+    let current = queue.shift();
+    for (let i = 0; i < queue.length; i++) {
+      if (current.price <= queue[i].price) {
+        answer[current.index]++;
+      } else {
+        answer[current.index]++;
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution3([1, 2, 3, 2, 3]));
