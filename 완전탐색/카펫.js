80c189c37c4209047cb6dc634dd82125a8560bc9
seok-hyung <korea970223@naver.com>
2023-12-30 21:46:40 +0900

    다른사람의 풀이 추가

--- 완전탐색/카펫.js
+++ 완전탐색/카펫.js
@@ -3,19 +3,36 @@ function solution(brown, yellow) {
   var answer = [];
   let sum = brown + yellow;
 
-  //카펫의 최소높이는 3부터이다.(위아래 갈색, 가운데가 노란색이기 때문에)
   for (let height = 3; height <= brown; height++) {
     //임의의 높이로 나눌때 나머지가 없을경우만
     if (sum % height === 0) {
-      //가로길이
-      let weight = sum / height;
+      let width = sum / height; //가로길이
 
       //테두리를 제외한 길이를 구해야하기 때문에 각각 -2해준뒤 곱셈
       //결과가 yellow와 같다면 해당 높이와 길이 리턴
-      if ((height - 2) * (weight - 2) === yellow) {
-        return [weight, height];
+      if ((height - 2) * (width - 2) === yellow) {
+        return [width, height];
       }
     }
+  }
+  return answer;
+}
+
+// 다른 사람의 풀이
+function solution(brown, red) {
+  var answer = [];
+  let row = 1,
+    col,
+    num;
+  while (true) {
+    col = Math.floor(red / row);
+    num = (row + 2) * 2 + col * 2;
+    if (num === brown && row >= col) {
+      answer[0] = row + 2;
+      answer[1] = col + 2;
+      break;
+    }
+    row++;
   }
   return answer;
 }
